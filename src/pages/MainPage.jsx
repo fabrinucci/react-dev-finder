228f64fec57a3aeeae1410d0b466a9c8b1a26aca
Fabrizio <fabrinucci99@gmail.com>
2023-06-22 21:19:13 -0300

    Fix background error

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react";
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import { AppContext } from "../context";
 import { Searcher } from "../components";
 import { UserCard } from "../containers";
@@ -12,28 +12,7 @@ export const MainPage = () => {
   }, [inputUser]);
 
   return (
-    <main
-      style={{
-        position: "relative",
-        backgroundColor: "rgba(56, 17, 94, 0.841)",
-        minHeight: "100vh",
-      }}
-    >
-      <Box
-        sx={{
-          content: '""',
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          backgroundImage: `url('/bg.webp')`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          opacity: 0.5,
-          zIndex: -1,
-        }}
-      ></Box>
+    <main>
       <Container
         sx={{
           display: "flex",
